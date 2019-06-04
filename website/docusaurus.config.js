d3ee5d218add40c4f53555274c7d7fd15e084ef1
Brian Kim <briankimpossible@gmail.com>
2019-06-04 17:08:51 -0400

    docusaurus stuff

---
+++ website/docusaurus.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  title: "Channel.js",
+  tagline: "The missing constructor for async iterators",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  themeConfig: {
+    navbar: {
+      title: "Channel.js",
+      logo: {
+        alt: "Channel.js logo",
+        src: "img/logo.svg",
+      },
+      links: [
+        { to: "docs/get-started", label: "Docs", position: "left" },
+        { to: "api", label: "API", position: "left" },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/channeljs/channel",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [],
+      logo: {},
+      copyright: `Copyright © ${new Date().getFullYear()} Brian Kim`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.json"),
+        },
+      },
+    ],
+  ],
+};
