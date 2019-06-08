855e99b9dd6ea208742d06b4009cb973da9a83db
Brian Kim <briankimpossible@gmail.com>
2019-06-08 06:02:04 -0400

    lint config files for no damn reason

--- rollup.config.js
+++ rollup.config.js
@@ -7,20 +7,18 @@ const pkg = require(path.join(root, "package.json"));
 export default {
   input: path.join(root, "./src/index.ts"),
   output: [
-    { 
+    {
       file: pkg.main,
       format: "cjs",
       sourcemap: true,
     },
-    { 
+    {
       file: pkg.module,
       format: "esm",
       sourcemap: true,
     },
   ],
-  plugins: [
-    typescript(),
-  ],
+  plugins: [typescript()],
   external: [
     "@channel/channel",
     "@channel/timers",
